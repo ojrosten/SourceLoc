a3ff76215de517e6cb49fdbb0d9bc64b80097a44
Oliver Rosten <oliver.rosten@gmail.com>
2023-04-12 18:17:48 +0100

    Simplify

--- Source/Location.cpp
+++
@@ -1,6 +0,0 @@
-#include "Location.hpp"
-
-std::string to_file(std::source_location loc)
-{
-  return loc.file_name();
-}
--- Source/Location.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <source_location>
-#include <string>
-
-std::string to_file(std::source_location loc = std::source_location::current());
